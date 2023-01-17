18f74b3c0f7d1f90b33f61fc968693cbaa4ab262
sultonqulov99 <sultonqulov99@gmail.com>
2023-01-17 17:32:32 +0500

    update

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8081
 const path = require('path')
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/login', (req, res) => {
+    console.log(path.join(__dirname, 'login.html'));
     res.sendFile(path.join(__dirname, 'login.html'))
 })
 app.get('/index', (req, res) => {
